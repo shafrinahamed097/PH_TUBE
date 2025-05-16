de9d6fe610e9c4743221fd107fee21704f02ae4a
shafrinahamed97 <shafrinahamedjob@gmail.com>
2025-05-16 23:28:00 +0600

    Load videos and display card on the UI

--- script/video.js
+++ script/video.js
@@ -20,6 +20,7 @@ const loadCategories = async () => {
 }
 
 
+
 // create DisplayCategories
 const displayCategories = (categories) => {
     const categoryContainer = document.getElementById("categories");
@@ -39,5 +40,52 @@ const displayCategories = (categories) => {
 
 
 
+// load videos
+const loadVideos = async () => {
+    try {
+        const res = await fetch("https://openapi.programming-hero.com/api/phero-tube/videos");
+        const data = await res.json();
+        const videos = data.videos;
+        displayVideos(videos);
+
+    }
+    catch (error) {
+        console.log("Videos Error:", error);
+    }
+
+}
+
+
+const displayVideos = (videos) => {
+    const videoContainer = document.getElementById("videos");
+    videos.forEach(video => {
+        const card = document.createElement("div");
+        card.classList = "card card-compact";
+        card.innerHTML = `
+        <figure>
+    <img
+      src=${video.thumbnail}
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">
+      Card Title
+      <div class="badge badge-secondary">NEW</div>
+    </h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <div class="badge badge-outline">Fashion</div>
+      <div class="badge badge-outline">Products</div>
+    </div>
+  </div>
+          
+        `;
+        videoContainer.append(card);
+
+    })
+}
+
+
 
 loadCategories();
+loadVideos();
