6a8af822bccfe9234fa89799bedf6e83b1d1552d
shafrinahamed97 <shafrinahamedjob@gmail.com>
2025-05-17 13:57:44 +0600

    Show conditional no content text for empty array

--- script/video.js
+++ script/video.js
@@ -97,6 +97,22 @@ const loadVideos = async () => {
 const displayVideos = (videos) => {
     const videoContainer = document.getElementById("videos");
     videoContainer.innerHTML = "";
+
+    if (videos.length == 0) {
+        videoContainer.classList.remove("grid");
+        videoContainer.innerHTML = `
+         <div class = "min-h-[300px] w-full flex flex-col gap-5 justify-center items-center">
+         <img src="assets/icon.png" />
+         <h2 class = "text-center text-xl font-bold">No Content Here in this Category</h2>
+         </div>
+        `;
+        return;
+
+    } else {
+        videoContainer.classList.add("grid");
+
+    }
+
     videos.forEach(video => {
         console.log(video);
         const card = document.createElement("div");
